ef27226f6021e4b3c667d064b9b3dd30eef62c65
TechMaximus1 <oladelevictor@gmail.com>
2024-07-17 12:22:22 +0100

    added footer to register and service

--- register.php
+++ register.php
@@ -3,3 +3,7 @@ require_once('header.php')
 $pagetitle = 'Register your account'
 ?>
 <h1>Register with FHBANK</h1>
+
+<?php
+    require_once("assets/footer.php");
+?>
--- service.php
+++ service.php
@@ -3,3 +3,8 @@
     require_once("assets/header.php");
 ?>
 <h1>service page</h1>
+
+
+<?php
+    require_once("assets/footer.php");
+?>
