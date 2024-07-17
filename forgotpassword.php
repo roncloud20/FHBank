86175e5ed7dfe1ed4cfae3aaec99c6239998fe8b
Adeosun John <adeosunadeyanju10@gmail.com>
2024-07-17 03:54:42 -0700

    Adding Forgot Password page

---
+++ forgotpassword.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $pagP?></title>
+</head>
+<body>
+    <h1>Forgot Password Page</h1>
+</body>
+</html>
+<?php
+    $pageP = "Forgot password";
+    require_once("assets/header.php");
+?>
