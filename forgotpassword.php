2ef1f830f9be2f394e8696a77aa4122456fe65ac
Adeosun John <adeosunadeyanju10@gmail.com>
2024-07-20 09:04:08 -0700

    Made changes to my forgotpassword page

--- forgotpassword.php
+++ forgotpassword.php
@@ -1,5 +1,28 @@
 <?php
     $pageTitle = "Forgot Password";
     require_once("assets/header.php");
+    $password = $cpassword = "";
+    $CpasswordE = $PasswordE = "";
+    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $password = $_POST['password'];
+        $cpassword = $_POST['cpassword'];
+        if(empty($password)){
+            $PasswordE = "Password is required";
+        }
+        if($password !== $cpassword){
+            $CpasswordE = "Password do not match";
+        }
+    }
 ?>
-<h1>Forgot Password Page</h1>
+<link rel="stylesheet" href="./forgotPassword.css">
+<div class="container">
+    <h1 class="myh1">Reset account Password</h1>
+    <p class="myp">Enter a new password</p>
+    <form method="post">
+        <input type="password" id="password" name="password" placeholder="New Password">
+        <span><?php echo "$PasswordE" ?></span>
+        <input type="password" id="cpassword" name="cpassword" placeholder="confirm New password">
+        <span><?php echo "$CpasswordE" ?></span> <br>
+        <button type="submit" value="submit" class="btn-reset"> Submit</button>
+    </form>
+</div>
