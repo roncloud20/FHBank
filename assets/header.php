3cdd0d52910d6c7b4f3711cb16c13453588a857a
TechMaximus1 <oladelevictor@gmail.com>
2024-07-28 15:40:39 +0100

    Added the Logo to the Header and styled with bootstrap

--- assets/header.php
+++ assets/header.php
@@ -75,7 +75,7 @@
     <header>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="index.php"><h1>FHBank&#174;</h1> <em><p>...Expanding your Financial Future</p></em></a>
+      <a class="navbar-brand" href="index.php"><img src="assets/images/bbb.jpg" height="50vh"></h1> <em><p>...Expanding your Financial Future</p></em></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
